c53bd05d490d39e1f842c7d4c211b5be9746fee7
MuhamadNadim <59022264+MuhamadNadim@users.noreply.github.com>
2020-11-09 14:51:20 +0800

    Added an = symbol to the script

--- LoginSystem/home.php
+++ LoginSystem/home.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['loggedin'])) {
 <div class="content">
 
 <h2>Home Page</h2>
-<p>Welcome to Emirates Airlines' Staff Homepage, <?$_SESSION['fullname']?>!</p>
+<p>Welcome to Emirates Airlines' Staff Homepage, <?=$_SESSION['fullname']?>!</p>
 
 </div>
 
