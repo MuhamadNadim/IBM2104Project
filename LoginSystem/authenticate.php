b9435b99ec2069575e8c97f8e9ffc790d535cecc
MuhamadNadim <59022264+MuhamadNadim@users.noreply.github.com>
2020-11-09 13:59:04 +0800

    Changed the SQL if statement's and the bind result's parameters.

--- LoginSystem/authenticate.php
+++ LoginSystem/authenticate.php
@@ -30,14 +30,14 @@ if ( !isset($_POST['email'], $_POST['password']) ) {
 	exit('<h3>Data does not exist! </h3>');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) {
+if ($stmt = $con->prepare('SELECT id, password, fullname FROM accounts WHERE email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['email']);
 	$stmt->execute();
 	// Store the result so we can check if the account exists in the database.
     $stmt->store_result();
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($id, $password);
+        $stmt->bind_result($id, $password,$fullname);
         $stmt->fetch();
         // Account exists, now we verify the password.
         // Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -47,6 +47,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) 
             // act like cookies but remember the data on the server.
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['fullname'];
             $_SESSION['id'] = $id;
             header('Location: home.php');
         } else {
