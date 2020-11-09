bcee4fa426ebe594b5d6696048827489e8bd9cd9
MuhamadNadim <59022264+MuhamadNadim@users.noreply.github.com>
2020-11-09 14:09:30 +0800

    Got data fetching scripst as I think it's redundant

--- LoginSystem/home.php
+++ LoginSystem/home.php
@@ -5,16 +5,6 @@ session_start();
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
-
-// We don't have the fullname info stored in sessions so 
-// instead we can get the results from the database.
-$stmt = $con->prepare('SELECT fullname FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($fullname);
-$stmt->fetch();
-$stmt->close();	
 }
 ?>
 
