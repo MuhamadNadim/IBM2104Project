8f243aa7e282533395624e2f399979bf48cb0ca5
MuhamadNadim <59022264+MuhamadNadim@users.noreply.github.com>
2020-11-10 13:58:42 +0800

    Deleted the database fetch scripts to prevent repetition and changed the variables' type from normal to session-based

--- LoginSystem/profile.php
+++ LoginSystem/profile.php
@@ -6,23 +6,6 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'employees';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the fullname, department, and job title info stored in sessions so 
-// instead we can get the results from the database.
-$stmt = $con->prepare('SELECT email, fullname, department, job_title FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($email, $fullname, $department, $job_title);
-$stmt->fetch();
-$stmt->close();
 ?>
 
 <html>
@@ -53,22 +36,22 @@ $stmt->close();
                     
 <tr>
 <td>Full Name:</td>
-<td><?=$fullname?></td>
+<td><?=$_SESSION['fullname']?></td>
 </tr>
                     
 <tr>
 <td>Work Email Address:</td>
-<td><?=$email?></td>
+<td><?=$_SESSION['email']?></td>
 </tr>
                     
 <tr>
 <td>Department:</td>
-<td><?=$department?></td>
+<td><?=$_SESSION['department']?></td>
 </tr>
 
 <tr>
 <td>Job Title:</td>
-<td><?=$job_title?></td>
+<td><?=$_SESSION['job_title']?></td>
 </tr>
                 
 </table>
